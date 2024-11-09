b084c40c02dc648a5567e6c8f122bc9732ecce7a
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2024-11-09 23:46:45 +0600

    six

---
+++ Chapter_2/celsiustofarhen.c
@@ -0,0 +1,9 @@
+//Write a program to convert Celsius to Fahrenheit. 
+#include<stdio.h>
+
+int main(){
+    float c=37.0;
+    float f=(9.0/5.0)*c+32;
+    printf("Your temparasor is %f",f);
+    return 0;
+}
