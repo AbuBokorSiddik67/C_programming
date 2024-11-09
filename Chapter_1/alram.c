ae44678e5def31b92ba72fda6d5b50339393cce1
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2024-11-09 23:02:05 +0600

    third

---
+++ Chapter_1/alram.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("Your alram is hear \a");
+    return 0;
+}
---
+++ Chapter_1/firstprogram1.c
@@ -0,0 +1,14 @@
+/*Write a program in c language print you Name, Roll, Collage Name etc in the box. 
+
+Show alarm our output. */
+#include<stdio.h>
+
+int main(){
+    printf("         Student history\n");
+    printf("__________________________________\n");
+    printf("| Student name: \t roll: \t Reg: |\n");
+    printf("| Sadikul islam \t 40000 \t 10203|\n");
+    printf("| Robiul islam  \t 40001 \t 10204|\n");
+    printf("|_________________________________|\n");
+    return 0;
+}
